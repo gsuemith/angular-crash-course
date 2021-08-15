2215b9e80aa076868964fe4557d254a536ab0b1d
Garrick Suemith <gsuemith@zotecpartners.com>
2021-08-15 17:19:51 -0400

    built add task form

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if(!this.text) {
+      alert('Please add a task!');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    // @todo - emit event
+    this.text = '';
+    this.day  = '';
+    this.reminder = false;
+  }
+}
