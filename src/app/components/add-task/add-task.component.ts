15da76faf726bc99d6141ed30efc266d03a1b7ef
Garrick Suemith <gsuemith@zotecpartners.com>
2021-08-15 17:27:57 -0400

    implemented add task functionality

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task'
+
 
 @Component({
   selector: 'app-add-task',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter()
   text!: string;
   day!: string;
   reminder: boolean = false;
@@ -27,7 +30,8 @@ export class AddTaskComponent implements OnInit {
       reminder: this.reminder
     }
 
-    // @todo - emit event
+    this.onAddTask.emit(newTask);
+    
     this.text = '';
     this.day  = '';
     this.reminder = false;
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -30,4 +30,10 @@ export class TasksComponent implements OnInit {
       .updateTaskReminder(task)
       .subscribe()
   }
+
+  addTask(task: Task) {
+    this.taskService
+      .addTask(task)
+      .subscribe((task) => (this.tasks.push(task)))
+  }
 }
